64d243107e8f72440a1d095a23e1c1d4cc3eaddd
vanshika <97521054+vanshika-max@users.noreply.github.com>
2024-01-22 06:18:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_convoperatn.js
@@ -0,0 +1,20 @@
+let score="33abc"
+console.log(typeof (score));
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+//value null=output 0
+//true=1 or false=0
+//undefined=nan
+//string =nan
+//"33"==33
+let loggedin=1
+let loggedisloggedin=Boolean(loggedin)
+console.log(loggedisloggedin);
+//1=true or 0 false
+//" "=false
+//"hitesh"=true
+let str="33"
+let stringnumber=String(str)
+console.log(stringnumber);
+console.log(typeof stringnumber)
