6ea38ed2b88599e7bdf3b8f2eb10922c200e2e0a
vanshika <97521054+vanshika-max@users.noreply.github.com>
2024-01-22 10:10:05 +0000

    comparison

--- 01_basics/03_convoperatn.js
+++ 01_basics/03_convoperatn.js
@@ -18,3 +18,23 @@ let str="33"
 let stringnumber=String(str)
 console.log(stringnumber);
 console.log(typeof stringnumber)
+//****************operations************/
+let value=3;
+let neg=-value
+// console.log(neg);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(5%2);
+
+let str1="hello "
+let str2="vanshu"
+let str3=str1+str2;
+console.log(str3);
+//adding of different datatypes gives ouptut differnet like this
+console.log("2"+1+1);
+console.log(1+2+"3")
+//that's why use paranthesis
+//+true=1
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(1>2);
+// console.log(1==1)
+// console.log("2">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+//reson of getting different outputs when with null beacause >,<and == works diffreenty <=,>=
+// as that treats as null =0
+//in undefiined spaces same 
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+//===
+console.log("2"===2);
