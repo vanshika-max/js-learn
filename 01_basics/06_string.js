a9afff73b13ade31150f410e6bd387295e74c40e
vanshika <97521054+vanshika-max@users.noreply.github.com>
2024-01-22 19:12:17 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,7 @@
+ const name="hitesh"
+ const repocount=50
+//  console.log(name+repocount+"value");
+//*******instead of that we use this format */
+ console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+ const gamename=new String('vanshu')
+console.log(gamename[0]);
