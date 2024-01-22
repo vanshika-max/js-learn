d7b1dbf3e5ada5b7d66159efb806dedf9b9ac057
vanshika <97521054+vanshika-max@users.noreply.github.com>
2024-01-22 06:01:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID=144455
+let accountEmail="vanshu@123.com"
+var accountpass="1233"
+accountcity="jaipur"
+let accountstate;
+//accountID=2//
+accountEmail="hxhvd.com"
+accountpass="11"
+accountcity="bengaluru"
+/*prefere not to use var use let
+beacause of functional scope issue */
+console.log(accountID);
+console.table([accountID,accountEmail,accountpass,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browser
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+let age=14
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
