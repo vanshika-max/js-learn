9e83632c91c88d6196b0611bdbef848bc1d427d5
vanshika <97521054+vanshika-max@users.noreply.github.com>
2024-02-01 07:53:26 +0000

    fun

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+if(true){
+    let a=10;
+    const b=20;
+    var c=30
+}
+function one(){
+    const username="vanshu"
+    function two(){
+        const web="youtube"
+        console.log(username);
+    }
+   
+    two()
+}
+one()
+
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
